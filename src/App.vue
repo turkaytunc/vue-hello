b231aad0cdf86ef67958068ac9992d93e86d0be4
Türkay Tunç <turkaytunc00@gmail.com>
2021-06-26 13:04:08 +0300

    remove: unused boilerplate codes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<template><HelloWorld msg="vue" /> </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
